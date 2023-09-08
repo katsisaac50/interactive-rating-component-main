104c2b8a717dee196d61a061a9f392ee79221a6d
katongole <katsisaac50@gmail.com>
2023-09-08 06:28:40 +0200

    added a button with in line sytling

--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,16 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div >
-      <h1 class="text-white">How did we do?</h1><br></br>
+      <h1 className="text-white">How did we do?</h1><br></br>
 
-      <p class="text-slate-400">Please let us know how we did with your support request. All feedback is appreciated 
+      <p className="text-slate-400">Please let us know how we did with your support request. All feedback is appreciated 
       to help us improve our offering!
       </p>
     
       1 2 3 4 5
     
-      Submit
+      <br/>
+      <button className="pt-1 pb-1 pl-20 pr-20 max-h-fit max-w-screen-2xl bg-orange-400 text-white  rounded-full ...">Submit</button>
       </div>
     </main>
   )
