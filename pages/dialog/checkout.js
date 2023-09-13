9145f816c8fcfcbe2e982b48aa925d8e120c0072
katongole <katsisaac50@gmail.com>
2023-09-13 09:47:23 +0200

    mobile implemented on home

--- pages/dialog/checkout.js
+++ pages/dialog/checkout.js
@@ -1,36 +1,41 @@
-import Head from 'next/head';
-import { useRating } from './ratingContext';
-import { StarSVG } from './images/StarSVG';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useRating } from '../ratingContext';
+import Layout from '../../components/layout';
+import { HeartSVG } from '../images/StarSVG';
 
 export default function Checkout() {
   const { rating } = useRating();
+  const router = useRouter();
+
+  useEffect(() => {
+    // Check if rating is null or undefined, and if so, navigate back to the Home component
+    if (rating === null || rating === undefined) {
+      router.replace('/'); // Replace with the correct path to the home page
+    }
+  }, [rating, router]);
+
+  // If rating is null or undefined, don't render anything
+  if (rating === null || rating === undefined) {
+    return null;
+  }
 
   return (
-    <>
-      <Head>
-        <title>Thank You</title>
-      </Head>
-      <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-24">
-        <div className="p-4 md:p-9 rounded-3xl bg-gray-800 max-w-[390px] mx-auto">
-          <div className="relative w-12 h-12 md:w-9 md:h-9 bg-gray-700 rounded-full p-4 md:p-5 shadow-xl">
-            <div className="absolute inset-0 flex justify-center items-center">
-              <StarSVG />
-            </div>
-          </div>
-          <h1 className="text-2xl md:text-3xl font-bold pt-4 md:pt-5 text-white sm:text-2xl">
-            Thank you!
-          </h1>
-          <br />
-          <label className="bg-slate-300 rounded-full px-4 py-2 ml-4 md:ml-12 text-orange-500">
-            You selected {rating ? rating : 0} out of 5
-          </label>
-          <br />
-          <p className="text-base md:text-lg text-gray-400 text-center md:text-left">
-            We appreciate you taking time to give a rating. If you ever need
-            more support, don't hesitate to get in touch!
-          </p>
+    <Layout>
+      <div className="p-4 md:p-9 rounded-3xl bg-gray-800 max-w-[390px] mx-auto text-center">
+        <div className="mb-4">
+          <HeartSVG className="w-16 h-16 text-orange-300 mx-auto" />
         </div>
-      </main>
-    </>
+        <label className="bg-slate-300 rounded-full px-4 py-2 text-orange-500">
+          You selected {rating} out of 5
+        </label>
+        <h2 className="text-white font-bold text-xl mt-4 mb-2">
+          Thank you!
+        </h2>
+        <p className="text-gray-400 text-sm">
+          We appreciate your feedback. If you need further assistance, feel free to get in touch with us.
+        </p>
+      </div>
+    </Layout>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -23,59 +23,62 @@ export default function Home() {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>First Post</title>
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-24">
-        <div className="p-4 md:p-9 rounded-3xl bg-gray-800 max-w-[390px] mx-auto">
-          <div className="relative w-12 h-12 md:w-9 md:h-9 bg-gray-700 rounded-full p-4 md:p-5 shadow-xl">
+        <div className="p-4 md:p-9 rounded-3xl bg-gray-800 max-w-xs md:max-w-[390px] mx-auto">
+          <div className="relative w-12 h-12 md:w-9 md:h-9 bg-gray-700 rounded-full p-2 md:p-3 shadow-xl">
             <div className="absolute inset-0 flex justify-center items-center">
               <StarSVG />
             </div>
           </div>
-          <h1 className="text-2xl md:text-3xl font-bold pt-4 md:pt-5 text-white sm:text-2xl">
+          <h1 className="text-xl md:text-3xl font-bold pt-2 md:pt-3 text-white">
             How did we do?
           </h1>
-          <br />
-          <p className="text-base md:text-lg leading-7 text-gray-300 text-sm md:text-base">
+          <p className="text-base md:text-lg leading-7 text-gray-300 text-sm md:text-base mt-2 md:mt-3">
             Please let us know how we did with your support request. All
             feedback is appreciated to help us improve our offering!
           </p>
-          <form onSubmit={onSubmit}>
-            <div className="py-4 md:py-8 flex flex-wrap justify-center space-x-3 md:space-x-7">
-              {ratingOptions.map((option) => (
-                <div key={option} className="w-1/2 md:w-auto">
-                  <input
-                    value={option}
-                    onChange={handleInputChange}
-                    id={`rate${option}`}
-                    className="peer/draft hidden"
-                    type="radio"
-                    name="status"
-                    checked={option === Number(rating)}
-                  />
-                  <label
-                    htmlFor={`rate${option}`}
-                    className={`cursor-pointer relative w-7 h-7 bg-gray-700 rounded-full flex justify-center items-center text-gray-300 text-center p-4 md:p-5 shadow-xl ${
-                      option === Number(rating)
-                        ? 'hover:bg-orange-600'
-                        : 'hover:bg-orange-600'
-                    }`}
-                  >
-                    {option}
-                  </label>
-                </div>
-              ))}
-            </div>
-            <button
-              disabled={!rating}
-              className={`pt-1 pb-1 w-full md:w-80 max-h-fit ${
-                rating ? 'hover:bg-gray-100 hover:text-orange-400' : ''
-              } max-w-screen-2xl bg-orange-400 text-white rounded-full ...`}
-              type="submit"
-            >
-              Submit
-            </button>
-          </form>
+          <form onSubmit={onSubmit} className="mt-4 md:mt-6">
+  <div className="py-2 md:py-4 flex justify-center space-x-3 md:space-x-7">
+    {ratingOptions.map((option) => (
+      <div key={option} className="flex items-center space-x-1">
+        <input
+          value={option}
+          onChange={handleInputChange}
+          id={`rate${option}`}
+          className="peer/draft hidden"
+          type="radio"
+          name="status"
+          checked={option === Number(rating)}
+        />
+        <label
+          htmlFor={`rate${option}`}
+          className={`cursor-pointer relative w-7 h-7 bg-gray-700 rounded-full flex justify-center items-center text-gray-300 text-center p-2 md:p-3 shadow-xl ${
+            option === Number(rating)
+              ? 'hover:bg-orange-600'
+              : 'hover:bg-orange-600'
+          }`}
+        >
+          {option}
+        </label>
+      </div>
+    ))}
+  </div>
+  <div className="flex justify-center mt-4 md:mt-6">
+    <button
+      disabled={!rating}
+      className={`pt-2 pb-2 w-3/4 md:w-80 max-h-fit ${
+        rating ? 'hover:bg-gray-100 hover:text-orange-400' : ''
+      } max-w-screen-2xl bg-orange-400 text-white rounded-full ...`}
+      type="submit"
+    >
+      Submit
+    </button>
+  </div>
+</form>
+
         </div>
       </main>
     </>
