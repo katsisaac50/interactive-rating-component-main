f44079d309f84ba31d63fb53f8a6268ee7ef551b
katongole <katsisaac50@gmail.com>
2023-09-13 02:56:37 +0200

    go to home if the rating is null

--- pages/dialog/checkout.js
+++ pages/dialog/checkout.js
@@ -1,7 +1,19 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useRating } from '../ratingContext';
+import Layout from '../../components/layout';
 import { HeartSVG } from '../images/StarSVG';
 
 export default function Checkout() {
   const { rating } = useRating();
+  const router = useRouter();
+
+  useEffect(() => {
+    // Check if rating is 0, and if so, navigate back to the Home component
+    if (rating === null) {
+      router.push('/');
+    }
+  }, [rating, router]);
 
   return (
     <Layout>
@@ -10,7 +22,7 @@ export default function Checkout() {
           <HeartSVG className="w-12 h-12 text-orange-300 mx-auto" />
         </div>
         <label className="bg-slate-300 rounded-full px-5 py-2 text-orange-500">
-          You selected {rating || 0} out of 5
+          You selected {rating} out of 5
         </label>
         <h2 className="text-white font-bold mt-5 mb-5">Thank you!</h2>
         <p className="text-gray-400">
