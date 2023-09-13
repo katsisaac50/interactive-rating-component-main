bf2a77f0b541c4793dcd8d1bd54e1b0ac4180bf4
katongole <katsisaac50@gmail.com>
2023-09-13 03:24:40 +0200

    modified route push

--- pages/index.js
+++ pages/index.js
@@ -21,29 +21,41 @@ export default function Home() {
     event.preventDefault();
     console.log('Rating:', rating);
     // Pass the inputValue as a query parameter
-    router.push({
-      pathname: '/dialog/checkout',
-      query: { rating: rating },
-    });
+    router.push(`/dialog/checkout?rating=${rating}`
+      // {pathname: '/dialog/checkout',
+      // query: { rating: rating },}
+    );
   };
+
+  const ratings = [1, 2, 3, 4, 5];
 
   return (
     <>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="p-9 rounded-3xl bg-gray-800 max-w-[390px] mx-auto">
-        <div className="relative w-9 h-9 bg-gray-700 rounded-full flex justify-center items-center  p-5 shadow-xl">
-        <StarSVG />
+    <Head>
+    <title>First Post</title>
+  </Head>
+  <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="p-9 rounded-3xl bg-gray-800 max-w-[390px] mx-auto">
+      <div className="relative w-9 h-9 bg-gray-700 rounded-full flex justify-center items-center  p-5 shadow-xl">
+        {/* Your SVG code */}
       </div>
-      <h1 className="text-3xl font-bold pt-5 text-white sm:text-2xl">How did we do?
-      </h1>
-      <br/>
-      <p className="text-lg leading-8 text-gray-300 text-sm md:text-base">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-          <form onSubmit={onSubmit}>
-          <div className="py-8 flex items-center justify-center space-x-7">
-          <input value="1" onClick={handleInputChange} id="rate1" className="peer/draft hidden" type="radio" name="status" checked />
+      <h1 className="text-3xl font-bold pt-5 text-white sm:text-2xl">How did we do?</h1>
+      <br />
+      <p className="text-lg leading-8 text-gray-300 text-sm md:text-base">
+        Please let us know how we did with your support request. All feedback is appreciated to help us improve our
+        offering!
+      </p>
+      <form onSubmit={onSubmit}>
+        <div className="py-8 flex items-center justify-center space-x-7">
+          <input
+            value="1"
+            onChange={handleInputChange}
+            id="rate1"
+            className="peer/draft hidden"
+            type="radio"
+            name="status"
+            checked={rating === '1'} // Update the checked condition
+          />
           <label htmlFor="rate1" className="cursor-pointer relative w-7 text-gray-300 h-7 bg-gray-700 rounded-full flex justify-center items-center text-center p-5 shadow-xl hover:bg-orange-600">1</label>
           <input value="2" onClick={handleInputChange} id="rate2" className="peer/draft hidden" type="radio" name="status" />
           <label htmlFor="rate2" className="cursor-pointer hover:bg-orange-600 relative w-7 h-7 bg-gray-700 rounded-full flex justify-center items-center text-gray-300 text-center p-5 shadow-xl">2</label>
@@ -54,16 +66,16 @@ export default function Home() {
           <input value="5" onClick={handleInputChange} id="rate5" className="peer/draft hidden" type="radio" name="status" />
           <label htmlFor="rate5" className="cursor-pointer  relative w-7 h-7 bg-gray-700 rounded-full flex justify-center items-center text-gray-300 text-center p-5 shadow-xl hover:bg-orange-600 focus:border-blue-400 ...">5</label>
         </div>
-            <button
-              disabled={!rating}
-              className={`pt-1 pb-1 w-80 max-h-fit ${rating && 'hover:bg-gray-100'} hover:text-orange-400 max-w-screen-2xl bg-orange-400 text-white rounded-full ...`}
-              type="submit"
-            >
-              Submit
-            </button>
-          </form>
-        </div>
-      </main>
-    </>
-  );
+        <button
+        disabled={!rating}
+        className={`pt-1 pb-1 w-80 max-h-fit ${rating && 'hover:bg-gray-100'} hover:text-orange-400 max-w-screen-2xl bg-orange-400 text-white  rounded-full ...`}
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
+  </div>
+</main>
+</>
+);
 }
