dcddb425393b45ff973eb978910dcbe5bbe74f55
katongole <katsisaac50@gmail.com>
2023-09-13 02:38:49 +0200

    Removed Unnecessary Imports: Removed the Image import, as it was not being used in this component.

--- pages/dialog/checkout.js
+++ pages/dialog/checkout.js
@@ -1,11 +1,7 @@
-import Image from 'next/image';
-import { useRating } from '../ratingContext';
-import './layout.module.css';
-import Layout from '../../components/layout';
 import { HeartSVG } from '../images/StarSVG';
 
-export default function Checkout(props) {
-  const { rating, setRating } = useRating();
+export default function Checkout() {
+  const { rating } = useRating();
 
   return (
     <Layout>
@@ -14,11 +10,11 @@ export default function Checkout(props) {
           <HeartSVG className="w-12 h-12 text-orange-300 mx-auto" />
         </div>
         <label className="bg-slate-300 rounded-full px-5 py-2 text-orange-500">
-          You selected {rating ? rating : 0} out of 5
+          You selected {rating || 0} out of 5
         </label>
         <h2 className="text-white font-bold mt-5 mb-5">Thank you!</h2>
         <p className="text-gray-400">
-          We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!
+          We appreciate your feedback. If you need further assistance, feel free to get in touch with us.
         </p>
       </div>
     </Layout>
