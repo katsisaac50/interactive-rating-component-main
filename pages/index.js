37ba18088f73e8a74769fe967baeb2eb70fe77e9
katongole <katsisaac50@gmail.com>
2023-09-13 04:32:28 +0200

    polish on the project

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'; // Use next/router instead of next/navigation
 import Head from 'next/head';
 
 import { Inter } from 'next/font/google';
+import { useRating } from './ratingContext';
+import { StarSVG } from './images/StarSVG';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,18 +14,22 @@ export const metadata = {
 };
 
 export default function Home() {
+  const { rating, setRating } = useRating();
   const router = useRouter();
-  const [rating, setRating] = useState('');
-  const ratings = [1, 2, 3, 4, 5]; // Define an array of ratings
+  const [inputValue, setInputValue] = useState('');
 
   const handleInputChange = (e) => {
     setRating(e.target.value);
   };
 
   function onSubmit(event) {
+    console.log('Rating:', rating);
     event.preventDefault();
-    router.push(`/dialog/checkout?rating=${rating}`);
+    router.push('/dialog/checkout', undefined, { shallow: true });
   }
+
+  // Generate an array of rating options
+  const ratingOptions = [1, 2, 3, 4, 5];
 
   return (
     <>
@@ -32,49 +38,57 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="p-9 rounded-3xl bg-gray-800 max-w-[390px] mx-auto">
-          <div className="relative w-9 h-9 bg-gray-700 rounded-full flex justify-center items-center  p-5 shadow-xl">
-            {/* Your SVG code */}
+          <div className="relative w-9 h-9 bg-gray-700 rounded-full p-5 shadow-xl">
+            <div className="absolute inset-0 flex justify-center items-center">
+              <StarSVG />
+            </div>
           </div>
-          <h1 className="text-3xl font-bold pt-5 text-white sm:text-2xl">How did we do?</h1>
+          <h1 className="text-3xl font-bold pt-5 text-white sm:text-2xl">
+            How did we do?
+          </h1>
           <br />
           <p className="text-lg leading-8 text-gray-300 text-sm md:text-base">
-            Please let us know how we did with your support request. All feedback is appreciated to help us improve our
-            offering!
+            Please let us know how we did with your support request. All
+            feedback is appreciated to help us improve our offering!
           </p>
           <form onSubmit={onSubmit}>
             <div className="py-8 flex items-center justify-center space-x-7">
-              {/* Map over the ratings array to generate radio buttons and labels */}
-              {ratings.map((rate) => (
-                <div key={rate}>
+              {ratingOptions.map((option) => (
+                <div key={option}>
                   <input
-                    value={rate.toString()}
+                    value={option}
                     onChange={handleInputChange}
-                    id={`rate${rate}`}
+                    id={`rate${option}`}
                     className="peer/draft hidden"
                     type="radio"
                     name="status"
-                    checked={rating === rate.toString()} // Check the radio button if rating matches
+                    checked={option === Number(rating)}
                   />
                   <label
-                    htmlFor={`rate${rate}`}
+                    htmlFor={`rate${option}`}
                     className={`cursor-pointer relative w-7 h-7 bg-gray-700 rounded-full flex justify-center items-center text-gray-300 text-center p-5 shadow-xl ${
-                      rating === rate.toString() && 'hover:bg-orange-600'
+                      option === Number(rating)
+                        ? 'hover:bg-orange-600'
+                        : 'hover:bg-orange-600'
                     }`}
                   >
-                    {rate}
+                    {option}
                   </label>
                 </div>
               ))}
             </div>
             <button
               disabled={!rating}
-              className={`pt-1 pb-1 w-80 max-h-fit ${rating && 'hover:bg-gray-100'} hover:text-orange-400 max-w-screen-2xl bg-orange-400 text-white  rounded-full ...`}
+              className={`pt-1 pb-1 w-80 max-h-fit ${
+                rating && 'hover:bg-gray-100'
+              } hover:text-orange-400 max-w-screen-2xl bg-orange-400 text-white  rounded-full ...`}
               type="submit"
             >
               Submit
             </button>
           </form>
         </div>
+        {/* <Checkout value={inputValue}/> */}
       </main>
     </>
   );
