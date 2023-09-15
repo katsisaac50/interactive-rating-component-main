ef489a7440f59f90d1a4882c0c68848c4dc3b6a7
katongole <katsisaac50@gmail.com>
2023-09-15 19:45:30 +0200

    configuring css to images

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/dialog/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './images/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
